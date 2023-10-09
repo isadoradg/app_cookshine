75bdc94294e8d388bc0a371a75beeb47405f0560
isadoradg <isahdomingos011@gmail.com>
2023-10-09 00:24:30 -0300

    Pasta View com as interfaces do aplicativo

---
+++ lib/view/tela_home.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import "package:flutter/material.dart";
+
+class TelaHome extends StatefulWidget {
+  const TelaHome({super.key});
+
+  @override
+  State<TelaHome> createState() => _TelaHomeState();
+}
+
+class _TelaHomeState extends State<TelaHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        
+        title: Text("COOKSHINE", style: TextStyle( color: Colors.black),),
+        /*Row(
+          children: [
+            Image.asset('lib/images/icon_kitchen_gradient.png'),
+            Text("COOKSHINE", style: TextStyle( color: Colors.black),),
+          ]
+        ),*/
+        backgroundColor: Color.fromARGB(255, 246, 244, 243),
+      
+      ),
+      
+      body: Center(
+        child: Container(
+          color: Color.fromARGB(255, 246, 244, 243),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                  Text("OOPS! NÃO FOI POSSÍVEL CARREGAR O FEED.", 
+                    style: TextStyle( 
+                      color: Colors.black
+                    ),
+                  ),
+                ]
+              ),
+              SizedBox(height: 20,),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                  Text("A página estava testando uma nova receita e perdeu-se na cozinha!", 
+                    style: TextStyle( 
+                      color: Colors.black
+                    ),
+                  ) ,
+                ]
+              ),
+              SizedBox(height: 20,),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                  Text("Mas por enquanto, entre ou cadastre-se para ver se a encontra por lá! E que tal testar uma nova receita também?", 
+                    style: TextStyle( 
+                      color: Colors.black
+                    ),
+                  ), 
+                ]
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
