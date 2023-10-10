0e1fdacafd96a888e090e1302c1425d159180858
isadoradg <isahdomingos011@gmail.com>
2023-10-09 22:31:02 -0300

    Ajustando rota para acessar a pagina de login

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@
 import 'package:flutter/material.dart';
 
 import 'view/tela_home.dart';
+import 'view/tela_login.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "cookshine",
+    
     initialRoute: '/cookshine',
-
     routes:{
       '/cookshine': (context) => TelaHome(),
+      '/login': (context) => TelaLogin(),
     },
+
   )
   );
 }
