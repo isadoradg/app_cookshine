3b37fdd9f7aeba27a62ed8339632aa46f6d598be
isadoradg <isahdomingos011@gmail.com>
2023-10-12 17:08:36 -0300

    Criação da tela de 'Cadastro'

---
+++ lib/view/tela_cadastro.dart
@@ -0,0 +1,135 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatefulWidget {
+  const TelaCadastro({super.key});
+
+  @override
+  State<TelaCadastro> createState() => _TelaCadastroState();
+}
+
+class _TelaCadastroState extends State<TelaCadastro> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+
+        backgroundColor: Color.fromARGB(255, 246, 244, 243),
+
+        actions:[
+          TextButton(
+            onPressed: () => Navigator.pop(context), 
+            child: Text("< VOLTAR", style: TextStyle( color: Colors.black)))
+        ]
+      ),
+
+      body:  Container(
+        color: Color.fromARGB(255, 246, 244, 243),
+        child: Center(
+          child: Container(
+            alignment: Alignment.center,
+            height: 400,
+            width: 400,
+            //color: Colors.grey[300],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('lib/images/icon_kitchen_gradient.png',height: 30,width: 30,),
+                    Text("COOKSHINE", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,),
+                  ],
+                ),
+                SizedBox(height: 20,),
+      
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("CADASTRE-SE!", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,)
+                ],),
+                SizedBox(height: 20,),
+
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
+                    Text("NOME"),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "ex.: Ana Maria",
+                      ),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                ),
+                SizedBox(height: 20,),
+      
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
+                    Text("E-MAIL"),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "ex.: anamaria@gmail.com",
+                      ),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                ),
+                SizedBox(height: 20,),
+      
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
+                    Text("SENHA"),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      obscureText: true,
+                    ),
+                ),
+                SizedBox(height: 20,),
+      
+                TextButton(
+                  onPressed:() => Navigator.pop(context),
+                  child: Text( "> PROSSEGUIR", 
+                    style: TextStyle( 
+                      color: Colors.black
+                    ), 
+                    textAlign: TextAlign.center,
+                  )
+                ),
+                SizedBox(height: 10,),
+      
+              ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
