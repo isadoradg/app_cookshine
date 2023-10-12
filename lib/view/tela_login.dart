996c27c91d5dab0962cc0ac613a0aa393a5e2557
isadoradg <isahdomingos011@gmail.com>
2023-10-12 17:12:41 -0300

    Ajuste na tela de Login -> insersão de validação do email e senha, troca do 'TextField' para 'TextFormField' e acrescentou no começo a opção de 'Form' no body/corpo da tela

--- lib/view/tela_login.dart
+++ lib/view/tela_login.dart
@@ -10,10 +10,33 @@ class TelaLogin extends StatefulWidget {
 }
 
 class _TelaLoginState extends State<TelaLogin> {
+  
+  // controladores
+  final controlEmail = TextEditingController();
+  final controlPassword = TextEditingController();
+
+  // funcao para validar se o campo esta vazio ou nao
+  /*validator(String value){
+    if(value.isEmpty){
+      return AlertDialog(
+        title: Text("Oops!"),
+        content: Text("Um dos campos não foi preenchido corretamente!"),
+        actions: [
+          TextButton(onPressed: () => Navigator.pop(context), child: Text("<FECHAR>"))
+        ],
+      );
+    }
+  }*/
+
+  // chave formulario
+  final _formKeyLogin = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
 
         backgroundColor: Color.fromARGB(255, 246, 244, 243),
 
@@ -24,43 +47,47 @@ class _TelaLoginState extends State<TelaLogin> {
         ]
       ),
 
-      body:  Container(
-        color: Color.fromARGB(255, 246, 244, 243),
-        child: Center(
-          child: Container(
-            alignment: Alignment.center,
-            height: 400,
-            width: 400,
-            //color: Colors.grey[300],
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('lib/images/icon_kitchen_gradient.png',height: 30,width: 30,),
-                    Text("COOKSHINE", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,),
-                  ],
-                ),
-                SizedBox(height: 20,),
-      
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("LOGIN", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,)
-                ],),
-                SizedBox(height: 20,),
-      
-                Row(
-                  children: [
-                    Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
-                    Text("E-MAIL"),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                  child: TextField(
+      body:  Form(
+        key: _formKeyLogin,
+        child: Container(
+          color: Color.fromARGB(255, 246, 244, 243),
+          child: Center(
+            child: Container(
+              alignment: Alignment.center,
+              height: 400,
+              width: 400,
+              //color: Colors.grey[300],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('lib/images/icon_kitchen_gradient.png',height: 30,width: 30,),
+                      Text("COOKSHINE", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+        
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("LOGIN", style: TextStyle( color: Colors.black), textAlign: TextAlign.center,)
+                  ],),
+                  SizedBox(height: 20,),
+        
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
+                      Text("E-MAIL"),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: controlEmail,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: "ex.: anamaria@gmail.com",
@@ -68,19 +95,31 @@ class _TelaLoginState extends State<TelaLogin> {
                       style: TextStyle(
                         color: Colors.black,
                       ),
-                    ),
-                ),
-                SizedBox(height: 20,),
+                      // validador de email
+                      validator: (value){
+                        bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!);
+                        if(value.isEmpty){
+                          return "Digite um e-mail.";
+                        }
+                        else if(!emailValid){
+                          return "Entre com um e-mail válido.";
+                        }
       
-                Row(
-                  children: [
-                    Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
-                    Text("SENHA"),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                  child: TextField(
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+        
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.fromLTRB(29, 0, 0, 0)),
+                      Text("SENHA"),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                    child: TextFormField(
+                      controller: controlPassword,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -88,55 +127,78 @@ class _TelaLoginState extends State<TelaLogin> {
                         color: Colors.black,
                       ),
                       obscureText: true,
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return "Digite uma senha.";
+                        }
+                      },
+      
                     ),
-                ),
-                SizedBox(height: 20,),
+                  ),
+                  SizedBox(height: 20,),
+        
+                  TextButton(
+                    //onPressed:() => Navigator.pop(context),
+                    child: Text( "> ENTRAR", 
+                      style: TextStyle( 
+                        color: Colors.black
+                      ), 
+                      textAlign: TextAlign.center,
+                    ),
       
-                TextButton(
-                  onPressed:() => Navigator.pop(context),
-                  child: Text( "> ENTRAR", 
-                    style: TextStyle( 
-                      color: Colors.black
-                    ), 
-                    textAlign: TextAlign.center,
-                  )
-                ),
-                SizedBox(height: 10,),
-
-                TextButton(
-                  onPressed:() => Navigator.pushNamed(context, '/esqueceu-a-senha'),
-                  child: Text( "ESQUECEU A SENHA?", 
-                    style: TextStyle( 
-                      color: Colors.black,
-                      fontSize: 10,
-                    ), 
-                    textAlign: TextAlign.center,
-                  )
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text("OU", 
-                    style: TextStyle( 
-                        color: Colors.black,
-                        fontSize: 10,
-                    ), 
-                    textAlign: TextAlign.center,)
-                  ],
-                ),
-                TextButton(
-                  onPressed:() => Navigator.pop(context),
-                  child: Text( "CADASTRE-SE!", 
-                    style: TextStyle( 
-                      color: Colors.black,
-                      fontSize: 10,
-                    ), 
-                    textAlign: TextAlign.center,
-                  )
-                ),
-
+                    onPressed: () {
+                      if(_formKeyLogin.currentState!.validate()){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Processando os dados"),)
+                        );
+                        controlEmail.clear();
+                        controlPassword.clear();
+                      }
+                    },
       
+                  ),
+                  SizedBox(height: 10,),
       
-              ]
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed:() => Navigator.pushNamed(context, '/esqueceu-a-senha'),
+                        child: Text( "ESQUECEU A SENHA?", 
+                          style: TextStyle( 
+                            color: Colors.black,
+                            fontSize: 10,
+                          ), 
+                          textAlign: TextAlign.center,
+                        )
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text("OU", 
+                          style: TextStyle( 
+                              color: Colors.black,
+                              fontSize: 10,
+                          ), 
+                          textAlign: TextAlign.center,)
+                        ],
+                      ),
+                      TextButton(
+                        onPressed:() => Navigator.pushNamed(context, '/cadastro'),
+                        child: Text( "CADASTRE-SE!", 
+                          style: TextStyle( 
+                            color: Colors.black,
+                            fontSize: 10,
+                          ), 
+                          textAlign: TextAlign.center,
+                        )
+                      ),
+                    ],
+                  ),
+      
+        
+        
+                ]
+              ),
             ),
           ),
         ),
