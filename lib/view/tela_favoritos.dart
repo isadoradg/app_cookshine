cd8139b6b463f55761b0ddf2fdc1f426596a9d49
isadoradg <isahdomingos011@gmail.com>
2023-10-24 02:17:50 -0300

    Tela inicial de itens/receitas marcadas como 'favorito'

---
+++ lib/view/tela_favoritos.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors, unused_field, prefer_final_fields
+
+import 'package:app_cookshine/model/receitas.dart';
+import 'package:flutter/material.dart';
+
+class TelaFavoritos extends StatefulWidget {
+  const TelaFavoritos({super.key});
+
+  @override
+  State<TelaFavoritos> createState() => _TelaFavoritosState();
+}
+
+class _TelaFavoritosState extends State<TelaFavoritos> {
+
+  List<Receitas> _salvos = [];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoritos', style: TextStyle( color: Colors.black)),
+        
+        //desativar o botão de voltar do AppBar
+        automaticallyImplyLeading: false,
+
+        backgroundColor: Color.fromARGB(255, 246, 244, 243),
+
+        actions:[
+          TextButton(
+            onPressed: () => Navigator.pop(context), 
+            child: Text("< VOLTAR", style: TextStyle( color: Colors.black)))
+        ]
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+            itemCount: _salvos.length,
+            itemBuilder: (context, index) { //percorre e cria uma lista com os nomes das receitas
+              return Card(
+                child: ListTile(
+                  title: Text(_salvos[index].receita),
+                  onTap: (){
+                    Navigator.pushNamed(context,'/receitas-detalhes', arguments: _salvos[index]);
+                  },
+                  /*trailing: Icon(
+                    jaFoiSalva ? Icons.star_rounded : Icons.star_border_rounded,
+                    color: jaFoiSalva ? Colors.yellowAccent[700] : null,
+                  ),*/
+                  
+                ),
+              );
+            },
+          ),
+      
+        ),
+    );
+  }
+}
+
+
+
