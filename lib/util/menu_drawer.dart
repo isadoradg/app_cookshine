b832979d2509074b61677a51886d62496f4ba8af
isadoradg <isahdomingos011@gmail.com>
2023-10-24 02:23:54 -0300

    Drawer inicial (menu) da tela inicial após o login do usuário - ainda está em teste

--- lib/util/menu_drawer.dart
+++ lib/util/menu_drawer.dart
@@ -1,23 +1,47 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_label, sort_child_properties_last
+
+//import "dart:js";
+
+//import "package:app_cookshine/util/constraints.dart";
 import "package:flutter/material.dart";
 
 var drawerMenu = Drawer(
-  backgroundColor: Colors.grey[300],
+  backgroundColor: Colors.grey[100],
   child: Column(
-    children: const[
-      //DrawerHeader(child:  )
+    children: [
+      DrawerHeader(child: Icon(Icons.favorite) ),
       ListTile(
         leading: Icon(Icons.soup_kitchen_outlined),
         title: Text("R E C E I T A S"),
+        onTap: (){
+          
+        },
       ),
       ListTile(
         leading: Icon(Icons.star),
         title: Text("F A V O R I T O S"),
+        onTap: (){
+
+        },
       ),
-      ListTile(
+      /*ListTile(
         leading: Icon(Icons.settings),
         title: Text("C O N F I G U R A Ç Õ E S"),
-        // onTap
+        onTap: (){
+
+        },
+      ),*/
+      ListTile(
+        leading: Icon(Icons.abc),
+        title: Text("S O B R E"),
+        onTap: () {
+          //Navigator.pushNamed(, '/sobre');
+        }
+        
       ),
     ],
   ),
 );
+
+
+
