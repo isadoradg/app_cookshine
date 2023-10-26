2ee9e1e0a3c9d9ca1e4efa88c249a136588c6048
isadoradg <isahdomingos011@gmail.com>
2023-10-26 17:49:52 -0300

    Criação de uma classe para adicionar novas receitas -> utilizada na tela_my_steps -> criação da própria biblioteca de receitas separada da que já vem montada do aplicativo

---
+++ lib/model/my_steps.dart
@@ -0,0 +1,17 @@
+
+// ignore_for_file: camel_case_types
+
+class MyStepsReceitas{
+  
+  String tituloReceita;
+  String ingredietesReceita;
+  String modoPreparoReceita;
+
+  MyStepsReceitas(
+    this.tituloReceita,
+    this.ingredietesReceita,
+    this.modoPreparoReceita,
+  );
+
+
+}
